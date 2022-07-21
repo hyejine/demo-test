3aaad74351c4a555393784e44adf2e38c5f79329
hyejine <hyejine1997@naver.com>
2022-07-21 17:29:45 +0900

    spring boot +mysql연동 test

---
+++ demo/src/main/java/model/dao/TestMapper.java
@@ -0,0 +1,9 @@
+package model.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface TestMapper {
+    
+}
---
+++ demo/src/main/java/model/dto/testDto.java
@@ -0,0 +1,16 @@
+package model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor //?
+@AllArgsConstructor //?
+public class testDto {
+    private String title;
+
+    private String content;
+}
